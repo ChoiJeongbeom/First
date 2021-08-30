a517376ed4089fea7d287fd396384b25d994af7e
JinEmperor <a01034881034@gmail.com>
2021-08-30 17:03:19 +0900

    git 두번째

--- first/src/first/Hello.java
+++ first/src/first/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!!!");
 		System.out.println("Hi~~" );
+		System.out.println("Hi~~" );
 	}
 }
---
+++ first/src/first/Second.java
@@ -0,0 +1,8 @@
+package first;
+
+public class Second {
+	public static void main(String[] args) {
+		System.out.println("bybybyby");
+		System.out.println("Hi~~" );
+     }
+}
